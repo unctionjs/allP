37ca1da612b034e52d1325d9dc87e2c276cb6937
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-11 17:11:17 -0700

    Woops, totally fucked up these paths

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import allObjectP from "./source"
+import allP from "./source"
 
 test(({same}) => {
   return allP([

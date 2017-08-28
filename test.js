7bb29e2a3db0e76349abf47af775ad366e26bc01
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 18:50:59 -0700

    Going back to how other javascript packages do it, makes it eaiser on everyone :/

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type */
 import {test} from "tap"
 
-import allP from "./source"
+import allP from "./"
 
 test(({same}) => {
   return allP([

0f32a7d1163e1ce05ed8856b5f652ad2d59e3c18
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:28 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-export default function allP (values: Array<any | Promise<any>>): Promise<Array<any>> {
-  return Promise.all(values)
+export default function allP(values) {
+  return Promise.all(values);
 }
